a5c848f9284850d3841e55d3258eb649b268389b
stephenparla <stephen.parla@gmail.com>
2026-02-28 03:53:05 -0500

    SpringAI:commit 3

---
+++ src/main/java/com/stephenparla/springai/controller/ChatController.java
@@ -0,0 +1,7 @@
+package com.stephenparla.springai.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+}
---
+++ src/main/java/com/stephenparla/springai/service/ChatService.java
@@ -0,0 +1,7 @@
+package com.stephenparla.springai.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+}
