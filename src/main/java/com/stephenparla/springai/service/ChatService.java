a38a52ce52a63f3c1d6e96a8af76e3d92eaf8931
stephenparla <stephen.parla@gmail.com>
2026-02-28 04:00:08 -0500

    SpringAI:commit 4

--- src/main/java/com/stephenparla/springai/service/ChatService.java
+++ src/main/java/com/stephenparla/springai/service/ChatService.java
@@ -1,7 +1,31 @@
 package com.stephenparla.springai.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.springai.chat.CompletionRequest;
+import com.springai.chat.CompletionResponse;
+import com.springai.chat.ChatAI;
 
 @Service
 public class ChatService {
+}
+
+
+
+@Service
+
+public class ChatCompletionService {
+
+    @Autowired
+
+    private ChatAI chatAI;
+
+    public String getChatCompletion(String userMessage) {
+
+        CompletionRequest request = new CompletionRequest(userMessage);
+
+        CompletionResponse response = chatAI.complete(request);
+
+        return response.getCompletion();
+    }
 }
