aba62198afbb885560be4fbabff789034e8636ac
stephenparla <stephen.parla@gmail.com>
2026-02-28 15:51:30 -0500

    SpringAI: Controller for user Message

--- src/main/java/com/stephenparla/springai/controller/ChatController.java
+++ src/main/java/com/stephenparla/springai/controller/ChatController.java
@@ -1,7 +1,21 @@
 package com.stephenparla.springai.controller;
 
+import com.stephenparla.springai.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/aichat")
 public class ChatController {
+
+    @Autowired
+    ChatService chatService;
+
+    @GetMapping("/{message}")
+    public String getFirstMessage(@PathVariable String userMessage) {
+        return chatService.getChatCompletion(userMessage);
+    }
 }
