13436edba90afeec08cad49b7910650770498073
stephenparla <stephen.parla@gmail.com>
2026-03-01 11:10:10 -0500

    SpringAI: Up and Running Version

--- src/main/java/com/stephenparla/springai/controller/ChatController.java
+++ src/main/java/com/stephenparla/springai/controller/ChatController.java
@@ -1,12 +1,14 @@
 package com.stephenparla.springai.controller;
 
 import com.stephenparla.springai.service.ChatService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/aichat")
 public class ChatController {
@@ -14,8 +16,14 @@ public class ChatController {
     @Autowired
     ChatService chatService;
 
-    @GetMapping("/{message}")
+    @GetMapping("/{userMessage}")
     public String getFirstMessage(@PathVariable String userMessage) {
-        return chatService.getChatCompletion(userMessage);
+        String response = "";
+        try {
+            response = chatService.getChatCompletion(userMessage);
+        } catch (Exception e){
+            log.error("Error:{}", e.getMessage());
+        }
+        return response;
     }
 }
--- src/main/java/com/stephenparla/springai/service/ChatService.java
+++ src/main/java/com/stephenparla/springai/service/ChatService.java
@@ -1,15 +1,22 @@
 package com.stephenparla.springai.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
+@Slf4j
 public class ChatService {
 
+    private final ChatClient chatClient;
+
     @Autowired
-    private ChatClient chatClient;
+    public ChatService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
     public String getChatCompletion(String userMessage) {
         return chatClient.prompt(userMessage).call().content();
