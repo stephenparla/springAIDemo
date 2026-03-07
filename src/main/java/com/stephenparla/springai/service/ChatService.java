14cd0af1f1c241accaf0660b759137c190134558
stephenparla <stephen.parla@gmail.com>
2026-03-06 23:21:37 -0500

    SpringAI: Config Changes to Disable default Anthropic and enable OpenAI.

--- src/main/java/com/stephenparla/springai/service/ChatService.java
+++ src/main/java/com/stephenparla/springai/service/ChatService.java
@@ -3,7 +3,6 @@ package com.stephenparla.springai.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
@@ -12,7 +11,6 @@ public class ChatService {
 
     private final ChatClient chatClient;
 
-    @Autowired
     public ChatService(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
